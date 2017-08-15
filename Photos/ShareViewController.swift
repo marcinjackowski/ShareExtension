7eb848b31c4db985303cbce2c78ab8a6eb333190
Marcin Jackowski <marcinjackowski@Marcins-MacBook-Pro.local>
2017-08-15 13:19:37 +0200

    open app after tap

--- Photos/ShareViewController.swift
+++ Photos/ShareViewController.swift
@@ -11,21 +11,26 @@ import Social
 
 class ShareViewController: SLComposeServiceViewController {
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        openApp()
     }
-
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    private func openApp() {
+        // 1
+        guard let url = NSURL(string: "shareextension://") else { return }
+        
+        // 2
+        let selector = sel_registerName("openURL:")
+        var responder = self as UIResponder?
+        
+        // 3
+        while let tempResponder = responder, !tempResponder.responds(to: selector)  {
+            responder = tempResponder.next
+        }
+        
+        // 4
+        _ = responder?.performSelector(inBackground: selector!, with: url)
     }
-
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
-
 }
